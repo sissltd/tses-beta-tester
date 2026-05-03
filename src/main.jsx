21a41d6e6cf291fef9d8e2ad5bbafdbade586ca6
SISSL <ssharpintelligence@gmail.com>
2026-05-03 14:40:49 +0100

    Initialize main.jsx with React setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TSESBetaSignup from "./TSESBetaSignup.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TSESBetaSignup />
+  </React.StrictMode>
+);
